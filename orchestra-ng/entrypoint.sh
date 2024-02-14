dbb07eb61e05e26811d190495d51e8a6b468baeb
marekd6 <37711928+marekd6@users.noreply.github.com>
2024-02-14 23:01:41 +0100

    Revert "Till advanced deployment"

--- orchestra-ng/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export ORCHESTRA_INSTRUMENT_URL
-export ORCHESTRA_SECTION_URL
-
-envsubst '${ORCHESTRA_SECTION_URL}, ${ORCHESTRA_INSTRUMENT_URL}' < /config.template > /etc/nginx/nginx.conf
-
-exec "$@"
